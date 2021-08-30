153259d1e6994f9a12b01a1e4f49535e1179dcea
osmar-jr <osmar.junior@zup.com.br>
2021-08-30 09:25:28 -0300

    Setup do Projeto Transações

---
+++ src/main/java/br/com/zupacademy/osmarjunior/OsmarJuniorApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.osmarjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmarJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmarJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/osmarjunior/OsmarJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.osmarjunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmarJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
